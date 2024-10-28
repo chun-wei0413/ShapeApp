f30fc86ca752af1ee157466b4dd5f218f17f7961
labsesdt <labsesdt@gmail.com>
2024-10-28 19:22:39 +0800

    21

--- src/main/java/org/ntut/posd2024f/midterm/DFSIterator.java
+++ src/main/java/org/ntut/posd2024f/midterm/DFSIterator.java
@@ -1,16 +1,24 @@
 package org.ntut.posd2024f.midterm;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class DFSIterator implements Iterator<Item>{
+    private List<Item> items;
     private Item item;
 
     public DFSIterator(Item item) {
+        items = new ArrayList<>();
         this.item = item;
     }
+    
     @Override
     public boolean hasNext() {
+        if(items.size() > 0){
+            return true;
+        }
         return false;
     }
 
