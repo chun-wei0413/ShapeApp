ba0ec83c151ceee79070163ba75b61c5a3410dbd
labsesdt <labsesdt@gmail.com>
2024-10-28 18:52:19 +0800

    16

--- src/main/java/org/ntut/posd2024f/midterm/DFSIterator.java
+++ src/main/java/org/ntut/posd2024f/midterm/DFSIterator.java
@@ -4,7 +4,11 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class DFSIterator implements Iterator<Item>{
+    private Item item;
 
+    public DFSIterator(Item item) {
+        this.item = item;
+    }
     @Override
     public boolean hasNext() {
         return false;
